ff5eab7cc2ff6d583840ece02d470fab3f88aca4
vladsundukov1-bit <vladsundukov1@gmail.com>
2026-04-18 16:32:20 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,36 +4,37 @@
 
 template<typename T, int size>
 class TStack {
-    private:
-    static const int k = size;
-    T data[k];
-    int top_index;
+ private:
+  static const int k = size;
+  T data[k];
+  int top_index;
 
-    public:
-    TStack() : top_index(-1) {}
-    void push(T value) {
-        if (top_index < k - 1) {
-            data[++top_index] = value;
-        }
-    }
+ public:
+  TStack() : top_index(-1) {}
 
-    T pop() {
-        if (top_index >= 0) {
-            return data[top_index--];
-        }
-        return T();
+  void push(T value) {
+    if (top_index < k - 1) {
+      data[++top_index] = value;
     }
+  }
 
-    T top() {
-        if (top_index >= 0) {
-            return data[top_index];
-        }
-        return T();
+  T pop() {
+    if (top_index >= 0) {
+      return data[top_index--];
     }
+    return T();
+  }
 
-    bool is_empty() {
-        return top_index == -1;
+  T top() {
+    if (top_index >= 0) {
+      return data[top_index];
     }
+    return T();
+  }
+
+  bool is_empty() {
+    return top_index == -1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
